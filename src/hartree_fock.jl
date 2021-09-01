500490a32d11f0f1ffd4e28bb2519bc46c51380b
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2021-09-02 02:50:55 +0300

    HF should be correct now

--- src/hartree_fock.jl
+++ src/hartree_fock.jl
@@ -19,19 +19,27 @@ function solve_hartree_fock(b::AbstractBasis, Vn ; Ve=x->exp(-x^2), rtol=1E-9, m
     orbitals = initial_orbitals(b)
     F = fock_matrix(b, orbitals; Vn=Vn)
     h₁ = one_electron_operator(b, Vn)
+
+    # We orthogonal, but not orthonormal basis
+    X = Diagonal( 1.0 ./ sqrt.( get_weight(b) ) )
+
     # Energy is h₁ + F (=2h₁+J) for two electrons
     E₀ = orbitals[:,1]' * ( h₁ + F ) * orbitals[:,1]
+
     @info "Energy at start = $(E₀)"
-    eo, orbitals = eigen(F)
+    eo, orbitals = eigen( X * F * X )
+    orbitals = X * orbitals
     for i in 1:max_iter
         F = fock_matrix(b, orbitals; Vn=Vn)
+        #fock_matrix!(F, b, orbitals; Vn=Vn)
         E₁ = orbitals[:,1]' * ( h₁ + F ) * orbitals[:,1]
         @info "Energy at iteration $i = $(E₁)"
         if abs(E₁-E₀) < rtol
             break
         else
             E₀ = E₁ 
-            eo, orbitals = eigen(F)
+            eo, orbitals = eigen( X * F * X )
+            orbitals = X * orbitals
         end
     end
     return eo, orbitals
