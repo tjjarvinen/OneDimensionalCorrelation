891a35a09ed7c0c5578eeac6fa59d0630c47cdd4
Teemu Järvinen <teemu.j.jarvinen@gmail.com>
2021-09-09 14:51:47 +0300

    full ci working

--- src/ci.jl
+++ src/ci.jl
@@ -13,17 +13,46 @@ function ci_matrix(b::AbstractBasis, orbitals::AbstractMatrix, Vn; Ve=x->exp(-x^
         ve[:,j] = Ve.(b[j].-b) 
     end
 
-    # Calculate Coulomb repulsion
     Threads.@threads for i in axes(ci,1)
         ϕ₁ = @view orbitals[:,indx[i][1]]
         ϕ₂ = @view orbitals[:,indx[i][2]]
         for j in axes(ci,2)
             ψ₁ = @view orbitals[:,indx[j][1]]
             ψ₂ = @view orbitals[:,indx[j][2]]
+            # Calculate Coulomb repulsion
             @tullio tmp = w[n] * ϕ₁[n] * ψ₁[n] * ve[n,m] * ϕ₂[m] * ψ₂[m] * w[m]
-            tmp += ϕ₁' * h1 * ψ₁ + ϕ₂' * h1 * ψ₂ 
+            # Orbitals are orthogonal
+            if indx[i][1] == indx[j][1] 
+                tmp += ϕ₂' * h1 * ψ₂ 
+            end
+            if indx[i][2] == indx[j][2]
+                tmp += ϕ₁' * h1 * ψ₁ 
+            end
             ci[i,j] = tmp
         end
     end
-    return ci
+    return ci, indx
+end
+
+
+function full_ci(b::AbstractBasis, orbitals::AbstractMatrix, Vn; Ve=x->exp(-x^2))
+    # We need to adjust threads so that we don't oversubscribe
+    bt = BLAS.get_num_threads()
+    nt = Threads.nthreads()
+    BLAS.set_num_threads(1)
+
+    ci, indx = ci_matrix(b, orbitals, Vn; Ve=Ve)
+
+    BLAS.set_num_threads(nt)
+
+    ev, ve = eigen(ci)
+
+    BLAS.set_num_threads(bt)
+
+    return Dict(
+        "orbitals"=>orbitals,
+        "energies"=>ev,
+        "states"=>ve,
+        "indexes"=>indx
+    )
 end
